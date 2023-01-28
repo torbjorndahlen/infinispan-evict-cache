3ad260e71b814e13d6ed9c1463d26931f2898cc6
torbjorndahlen <torbjorndahlen@icloud.com>
2023-01-28 14:36:23 +0100

    Added Hotrod client

---
+++ ServerTask/client/src/main/java/example/CacheServerTaskInvocation.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+
+public class CacheServerTaskInvocation {
+
+  private static Logger logger = Logger.getLogger(CacheServerTaskInvocation.class.getName());
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    ConfigurationBuilder remoteBuilder = new ConfigurationBuilder();
+    remoteBuilder.addServers(args[0]);
+    remoteBuilder.security().authentication().username(args[1]).password(args[2]);
+//    remoteBuilder.marshaller(new JavaSerializationMarshaller()).addJavaSerialWhiteList("java.util.*"); // needed because of ISPN-14131
+
+    RemoteCacheManager remoteCacheManager = new RemoteCacheManager(remoteBuilder.build(), true);
+
+    // obtain a handle to the remote default cache
+    RemoteCache<String, String> customCache = remoteCacheManager.getCache("rpi-store");
+
+    Object returnFromTask = customCache.execute("EvictReloadTask");
+    
+    System.out.println("Returned from Task : " + returnFromTask);
+
+    
+  }
+
+}
